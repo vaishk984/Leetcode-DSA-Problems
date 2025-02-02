9a3c28a8db233d0b50c2f7fe54fe906bb0b3f591
vaishk984 <111275427+vaishk984@users.noreply.github.com>
2025-02-02 12:23:43 +0530

    Time: 2 ms (99.14%) | Memory: 42.8 MB (90.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -23,4 +23,17 @@ class Solution {
 
         return true;
     }
+
+    public static boolean palindrome(int i, String s){
+        
+        if(i>=s.length()/2){
+            return true;
+        }
+
+        if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+            return false;
+        }
+
+        return palindrome(i+1, s);
+    }
 }
