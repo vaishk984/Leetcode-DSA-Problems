a316c247d95d38dc24dc395b9a0689bdfef723e3
vaishk984 <111275427+vaishk984@users.noreply.github.com>
2025-05-17 19:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (63.97%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,66 +1,29 @@
 class MyStack {
-    private Queue<Integer> q1;
-    private Queue<Integer> q2;
+
+    private Queue<Integer> q;
 
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q = new LinkedList<>();
     }
     
     public void push(int x) {
-        if(!q1.isEmpty()){
-            q1.add(x);
-        }else{
-            q2.add(x);
+        q.add(x);
+
+        for(int i = 0; i<q.size()-1; i++){
+            q.add(q.remove());
         }
     }
     
     public int pop() {
-        int top = -1;
-        //Case 1
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                top = q1.remove();
-                if(q1.isEmpty()){
-                    break;
-                }
-                q2.add(top);
-            }
-        }else{
-            //Case 2
-            while(!q2.isEmpty()){
-                top = q2.remove();
-                if(q2.isEmpty()){
-                    break;
-                }
-                q1.add(top);
-            }
-        }
-
-        return top;
+        return q.remove();
     }
     
     public int top() {
-        
-            int top = -1;
-
-            //case 1
-            if(!q1.isEmpty()){
-                while(!q1.isEmpty()){
-                    top = q1.remove();
-                    q2.add(top);
-                }
-            }else{
-                while(!q2.isEmpty()){
-                    top = q2.remove();
-                    q1.add(top);
-                }
-            }
-            return top;
+        return q.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
+        return q.isEmpty();
     }
 }
 
