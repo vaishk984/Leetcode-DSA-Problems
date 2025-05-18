45c4f7fcaf3594f102a79707fd99f75799eeba09
vaishk984 <111275427+vaishk984@users.noreply.github.com>
2025-05-18 20:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,7 +5,7 @@ public:
             return false;
         }
 
-        vector<int> freq(26, 0);
+        int freq[26] = {0};
         
         for(int i = 0; i < s.length(); i++) {
             freq[s[i]-'a']++;
