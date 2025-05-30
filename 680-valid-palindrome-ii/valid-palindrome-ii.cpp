211ce4832ff6652af9191ddd331e36ab5e8ff79a
vaishk984 <111275427+vaishk984@users.noreply.github.com>
2025-05-30 17:40:36 +0530

    Time: 8 ms (27.16%) | Memory: 26.7 MB (22.97%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(const string &s, int i, int j){
+    bool isPalindrome(string s, int i, int j){
         while(i<j){
-            if(s[i++] != s[j--])
-                return false;
-            
-        }
+            if(s[i++] != s[j--]) return false;
+        } 
 
         return true;
     }
     bool validPalindrome(string s) {
-        int i = 0;
+        int i = 0; 
         int j = s.size()-1;
-        while(i<j){
-            if(s[i]!=s[j])
-                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
-            ++i; --j;
-        }
 
+        while(i<j){
+            if(s[i] != s[j]){
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+            i++;
+            j--;
+        }
         return true;
     }
 };
