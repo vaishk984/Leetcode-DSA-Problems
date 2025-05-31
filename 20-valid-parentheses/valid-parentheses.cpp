816d868c571a1d3afb8374d80f759fda72515f07
vaishk984 <111275427+vaishk984@users.noreply.github.com>
2025-05-31 21:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (34.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(char ch: s){
-            //opening
-            if(ch=='(' || ch=='[' || ch=='{') st.push(ch);
 
-            //closing
-            else{
-                if(st.empty()) return false;
-                
-                if((st.top() == '(' && ch==')') || (st.top() == '[' && ch==']') || (st.top()=='{' && ch=='}')){
-                    st.pop();
-                }
-                else{
+        for(char ch: s){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }else{
+                if(st.empty()){
                     return false;
+                }else{
+                    if((st.top()=='(' && ch==')') || (st.top()=='[' && ch==']') || (st.top()=='{' && ch=='}')){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
                 }
             }
         }
 
         if(st.empty()){
             return true;
-        }
-        else{
+        }else{
             return false;
         }
+        
     }
 };
