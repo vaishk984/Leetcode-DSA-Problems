9fa769ea2045a4d4141c2c41e8b956fce24fd929
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-06-17 22:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (40.23%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {}; 
+        if(digits.empty()) return {};
         vector<string> phoneMap = {
-            "", "", "abc", 
-            "def", "ghi", "jkl", 
-            "mno", "pqrs", "tuv", "wxyz"
+            "", "", "abc", "def",
+            "ghi", "jkl", "mno",
+            "pqrs", "tuv", "wxyz"
         };
 
         string path = "";
@@ -16,17 +16,18 @@ public:
         return result;
     }
 
-    void getCombinations(string &digits, int index, string &path, vector<string> &result, vector<string> &phoneMap){
+    void getCombinations(string digits, int index,string &path, vector<string> &result, vector<string> &phoneMap){
         if(index==digits.size()){
             result.push_back(path);
             return;
         }
 
         string possibleLetters = phoneMap[digits[index] - '0'];
+
         for(char c: possibleLetters){
             path.push_back(c);
             getCombinations(digits, index+1, path, result, phoneMap);
             path.pop_back();
         }
-    }
+    }   
 };
