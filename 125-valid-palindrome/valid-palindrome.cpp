2da9bbb1953250ba3a5f4872311fd7b3b1f28c41
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-07-08 16:02:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (36.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,16 +3,17 @@ public:
     bool isPalindrome(string s) {
         string new_str = "";
         
-        for(char c: s){
+        for(char c : s){
             if(isalnum(c)){
                 new_str += tolower(c);
             }
         }
 
         int i = 0;
-        int j = new_str.size()-1;
-        while(i<j){
-            if(new_str[i]!=new_str[j]){
+        int j = new_str.size() - 1;
+
+        while(i<=j){
+            if(new_str[i] != new_str[j]){
                 return false;
             }
 
