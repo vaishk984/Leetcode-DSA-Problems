323fd9047e945970b3eaae650bc7825fcb73baa1
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-07-17 05:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (50.89%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
-    vector<vector<int>> zeroMatrix(vector<vector<int>>& matrix, int n, int m){
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool firstRow = false;
+        bool firstCol = false;
 
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
+            if(matrix[i][0] == 0){
+                firstCol = true;
+            }
+        }
+        
+        for(int j = 0; j<m; j++){
+        
+            if(matrix[0][j] == 0){
+                firstRow = true;
+            }
+        }
+
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
                 if(matrix[i][j]==0){
-                    row[i] = 1;
-                    col[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(row[i] == 1 || col[j]== 1){
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        return matrix;
-    }
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        zeroMatrix(matrix, n, m);
-        
+        if(firstRow){
+            for(int j = 0; j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstCol){
+            for(int i = 0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
