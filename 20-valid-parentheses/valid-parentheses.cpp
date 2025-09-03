3764d1bc64a694da4c0a8503ae76d82e0d898128
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-09-04 02:11:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.75%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for(char ch: s){
-            if(ch=='(' || ch=='[' || ch=='{'){
-                st.push(ch);
+        for(char c : s){
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
             }else{
                 if(st.empty()){
                     return false;
                 }else{
-                    if((st.top()=='(' && ch==')') || (st.top()=='[' && ch==']') || (st.top()=='{' && ch=='}')){
+                    char top = st.top();
+                    if((top=='(' && c==')') || (top=='[' && c==']') || (top=='{' && c=='}')){
                         st.pop();
                     }else{
                         return false;
@@ -19,11 +19,10 @@ public:
             }
         }
 
-        if(st.empty()){
-            return true;
-        }else{
+        if(!st.empty()){
             return false;
         }
-        
+
+        return true;
     }
 };
