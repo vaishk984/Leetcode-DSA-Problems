dbbf15138be556b3d25dfc54d40e3675724932dd
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-10-20 22:28:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (80.43%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void getAllSubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allSubsets){
-        if(i==nums.size()){
-            allSubsets.push_back(ans);
+    void getAllSubsets(vector<int>& nums, int i, vector<int>& ans, vector<vector<int>>& allSubsets){
+        if(i == nums.size()){
+            allSubsets.push_back({ans});
             return;
         }
 
         ans.push_back(nums[i]);
-        getAllSubsets(nums, ans, i+1, allSubsets);
+        getAllSubsets(nums, i+1, ans, allSubsets);
         ans.pop_back();
-        getAllSubsets(nums, ans, i+1, allSubsets);
+        getAllSubsets(nums, i+1, ans, allSubsets);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> allSubsets;
         vector<int> ans;
 
-        getAllSubsets(nums, ans, 0, allSubsets);
+        getAllSubsets(nums, 0, ans, allSubsets);
 
         return allSubsets;
     }
