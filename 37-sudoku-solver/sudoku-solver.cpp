2d0ec5e6bcec1f0dd3afec86cf6762b0f09288ca
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-10-22 21:31:06 +0530

    Time: 223 ms (71.77%) | Memory: 8.6 MB (99.50%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>> &board, int row, int col, int dig){
+    bool isSafe(vector<vector<char>>& board, int row, int col, char dig){
+        //horizontal
+
+        for(int j = 0; j<9; j++){
+            if(board[row][j] == dig){
+                return false;
+            }
+        }
+
         //vertical
+
         for(int i = 0; i<9; i++){
             if(board[i][col] == dig){
                 return false;
             }
         }
 
-        //horizontal
-        for(int j = 0; j<9; j++){
-            if(board[row][j] == dig){
-                return false;
-            }
-        }   
+        //Grid
 
-        //grid
-        int srow = (row/3) * 3;
-        int scol = (col/3) * 3;
-
+        int srow = (row/3)*3, scol = (col/3)*3;
         for(int i = srow; i<=srow+2; i++){
             for(int j = scol; j<=scol+2; j++){
-                if(board[i][j]==dig){
+                if(board[i][j] == dig){
                     return false;
                 }
             }
@@ -37,7 +38,7 @@ public:
 
         int nextRow = row, nextCol = col + 1;
 
-        if(nextCol == 9){
+        if(nextCol==9){
             nextRow = row + 1;
             nextCol = 0;
         }
@@ -49,16 +50,18 @@ public:
         for(char dig = '1'; dig<='9'; dig++){
             if(isSafe(board, row, col, dig)){
                 board[row][col] = dig;
+
                 if(helper(board, nextRow, nextCol)){
                     return true;
                 }
 
                 board[row][col] = '.';
-            }
+            }   
         }
 
         return false;
     }
+
     void solveSudoku(vector<vector<char>>& board) {
         helper(board, 0, 0);
     }
