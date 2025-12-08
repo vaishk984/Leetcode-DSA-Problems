b74f7e14ca387ea0dfd0b0f93fd02502e9741aea
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-12-08 18:08:55 +0530

    Time: 51 ms (56.15%) | Memory: 90.8 MB (59.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,13 +4,13 @@ public:
         unordered_set<int> set;
 
         for(int i = 0; i<nums.size(); i++){
-            if(set.find(nums[i]) != set.end()){
+        if(set.find(nums[i]) != set.end()){
                 return true;
             }
 
             set.insert(nums[i]);
         }
 
-        return false;
+        return false;    
     }
 };
