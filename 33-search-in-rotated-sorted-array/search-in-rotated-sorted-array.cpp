1c606e3bd11aad7334ac4cfedc444b020011edac
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2025-12-30 01:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.03%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-
-    int binaryRS(vector<int>& nums, int st, int en, int target){
+    int search(vector<int>& nums, int target) {
+        int st = 0, en = nums.size() - 1;
         while(st<=en){
             int mid = st + (en-st)/2;
 
-            if(nums[mid] == target) return mid;
+            if(nums[mid] == target) {
+                return mid;
+            }
 
             if(nums[st] <= nums[mid]){
                 if(target>=nums[st] && nums[mid]>=target){
-                    return binaryRS(nums, st, mid-1, target);
+                    en = mid - 1;
                 }else{
-                    return binaryRS(nums, mid+1, en, target);
+                    st = mid + 1;
                 }
             }else{
                 if(target>=nums[mid] && target<=nums[en]){
-                    return binaryRS(nums, mid+1, en, target);
+                    st = mid + 1;
                 }else{
-                    return binaryRS(nums, st, mid-1, target);
+                    en = mid - 1;
                 }
             }
         }
 
         return -1;
-        
-    }
-
-    int search(vector<int>& nums, int target) {
-        return binaryRS(nums, 0, nums.size()-1, target);
     }
 };
