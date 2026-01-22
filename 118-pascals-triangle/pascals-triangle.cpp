be0ff7752fe39a44d536a7e4b9d80b6fad77cbe8
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2026-01-22 20:16:41 +0530

    Time: 3 ms (7.99%) | Memory: 9.6 MB (63.48%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> generateRow(int row){
         long long ans = 1;
         vector<int> ansRow;
-
-        ansRow.push_back(ans);
+        ansRow.push_back(1);
 
         for(int col = 1; col<row; col++){
-            ans = ans * (row-col);
+            ans = ans * (row - col);
             ans = ans/col;
             ansRow.push_back(ans);
         }
@@ -16,10 +15,9 @@ public:
     }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-
         for(int i = 1; i<=numRows; i++){
             ans.push_back(generateRow(i));
-        }
+        }   
 
         return ans;
     }
