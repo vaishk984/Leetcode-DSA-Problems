ed373744daff1facec7d31fc05fbeacdb56680ba
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2026-02-01 03:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (43.44%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
-    bool searchInRow(vector<vector<int>>& matrix, int target, int row){
-        int start = 0, end = matrix[0].size()-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(matrix[row][mid] == target){
-                return true;
-            }else if(matrix[row][mid]<target){
-                start = mid + 1;
-            }else{
-                end = mid - 1;
-            }
-        }
-
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
+        int n = matrix.size(), m = matrix[0].size();
+        int low = 0, high = (n*m) - 1;
 
-        int startRow = 0, endRow = m-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            int row = mid / m, col = mid % m;
 
-        while(startRow <= endRow){
-            int midRow = startRow + (endRow-startRow)/2;
-
-            if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]){
-                return searchInRow(matrix, target, midRow);
-            }else if(target>matrix[midRow][n-1]){
-                startRow = midRow + 1;
-            }else{
-                endRow = midRow - 1;
-            }
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
 
         return false;
