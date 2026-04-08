f6a70b4664ee816d507d6251adf15dc03a12d6de
Kumari Vaishnavi <111275427+vaishk984@users.noreply.github.com>
2026-04-08 18:58:46 +0530

    Time: 2 ms (91.09%) | Memory: 48 MB (27.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,7 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int j = -1;
-
         for(int i = 0; i<nums.length; i++){
             if(nums[i] == 0){
                 j = i;
@@ -9,20 +8,15 @@ class Solution {
             }
         }
 
-        if(j==-1){
-            return;
-        }
+        if(j == -1) return;
 
         for(int i = j+1; i<nums.length; i++){
-            if(nums[i]!=0){
+            if(nums[i] != 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-
                 j++;
             }
         }
-
-        return;
     }
 }
